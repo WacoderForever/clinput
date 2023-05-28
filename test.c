3e54f8645e948f7ac3032d93bd73a35e5b84d5d4
WacoderForever <sethomondi088@gmail.com>
2023-05-29 01:09:19 +0300

    hgf

--- test.c
+++ test.c
@@ -8,14 +8,14 @@
 long main(){
 
    CliInterface  interface = newCliInterface();
-
+    interface.ask_color=CLI_YELLOW;
 
 
     // long k=interface.ask_long(&interface,"type integer");
    // double z=interface.ask_double(&interface,"type double");
-
+  interface.wrong_option_menssage="thats wrong";
     int chosed = interface.ask_option(&interface,"type the option" ,"aaa | aaa | bbb | lll");
-
+  
     printf("%i\n",chosed);
     return 0;
 }
