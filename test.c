0cc12a7a7b3b9b9dc2446fa230178ab20e58fcbd
WacoderForever <sethomondi088@gmail.com>
2023-05-29 17:16:22 +0300

    tk

--- test.c
+++ test.c
@@ -5,10 +5,16 @@
 #include "clinput/clinput.h"
 
 long main(){
-   CliInterface interface=newCliInterface();
-    interface.success_color=CLI_MAGENTA;
-    long age=interface.ask_long(&interface,"what is your age");
+  CliInterface  interface = newCliInterface();
+
+   char *name = interface.ask_string(&interface,"type your name",CLI_TRIM);
+   long age=interface.ask_long(&interface,"what is your age");
+   double weight=interface.ask_double(&interface,"what your weight");
+
+    printf("name: %s\n",name);
     printf("age %li\n",age);
+    printf("weight %lf\n",weight);
+
     return 0;
    
 }
